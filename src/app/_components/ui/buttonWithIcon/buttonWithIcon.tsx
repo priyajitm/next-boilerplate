3149811c6a2dc5836c4e9398408c79a81dcc95f2
Priyajit Mukherjee <priyajit@founderandlightning.com>
2024-12-22 20:12:35 +0530

    chore: added variant

--- src/app/_components/ui/buttonWithIcon/buttonWithIcon.tsx
+++ src/app/_components/ui/buttonWithIcon/buttonWithIcon.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Button } from "../../shadcnComponents/button";
 
+type ButtonVariant =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
+
 interface ButtonWithIconProps {
   icon: React.ReactNode;
   label: string;
+  variant: ButtonVariant;
 }
 
-export const ButtonWithIcon = ({ icon, label }: ButtonWithIconProps) => {
+export const ButtonWithIcon = ({
+  icon,
+  label,
+  variant = "default",
+}: ButtonWithIconProps) => {
   return (
-    <Button className="flex w-full items-center gap-2">
+    <Button className="flex w-full items-center gap-2" variant={variant}>
       {icon}
       {label}
     </Button>
